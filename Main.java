500b010263c929f7ba9d26158d89391dff385adb
dinathebanana <dqphung101@student.hbuhsd.edu>
2025-10-07 21:46:11 +0000

    meow

--- Main.java
+++ Main.java
@@ -6,10 +6,33 @@ public class Main
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
+    // Problem 1
+    System.out.println("Enter in a number");
+    double target = 12.345;
+    double user_input = scan.nextDouble();
+    
 
+    if (user_input == target)
+    {
+      System.out.println("YES!");  
+    }
+    else
+    {
+      System.out.println("NO!");
+    }
 
+    System.out.println("Please eneter an interger:");
+    int score = 48;
+    int user_input2 = scan.nextInt();
 
-
+    if (user_input2 == score)
+    {
+      System.out.println("YEAH");
+    }
+    else
+    {
+      System.out.println("NAH");
+    }
     /* ============= END SOLUTION ================================== */
     scan.close();
   }
